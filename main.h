fc5b7dcd354ec09773426e190d724102f99684b8
Glorycornel <104757918+Glorycornel@users.noreply.github.com>
2022-07-12 13:52:58 +0100

    Update main.h
    1st commit

--- main.h
+++ main.h
@@ -1,70 +1,120 @@
-#ifndef _MAIN_
-#define _MAIN_
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
+#ifndef _PRINTF_H_
+#define _PRINTF_H_
 
-#define STR_TO_INT(val) (val - 48)
-#define INT_TO_STR(val) (val + 48)
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdlib.h>
+
+#define OUTPUT_BUF_SIZE 1024
+#define BUF_FLUSH -1
+
+#define FIELD_BUF_SIZE 50
+
+#define NULL_STRING "(null)"
+
+#define PARAMS_INIT {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+
+#define CONVERT_LOWERCASE	1
+#define CONVERT_UNSIGNED	2
+
 /**
- * struct ops - Struct ops
- * @c: The character associated to this op
- * @op: A callback function to perform the print
- * operation.
+ * struct parameters - parameters struct
+ *
+ * @unsign: flag if unsigned value
+ *
+ * @plus_flag: on if plus_flag specified
+ * @space_flag: on if hashtag_flag specified
+ * @hashtag_flag: on if _flag specified
+ * @zero_flag: on if _flag specified
+ * @minus_flag: on if _flag specified
+ *
+ * @width: field width specified
+ * @precision: field precision specified
+ *
+ * @h_modifier: on if h_modifier is specified
+ * @l_modifier: on if l_modifier is specified
+ *
  */
-typedef struct ops
+
+typedef struct parameters
 {
-	char c;
-	int (*op)(int, char *, va_list);
-} op_t;
-int _printf(const char *format, ...);
-int (*get_print_function(char *format))(int, char *, va_list);
-int _putchar(char c);
-int print_char(int n, char *format, va_list args);
-int print_string(int n, char *format, va_list args);
-int print_int(int n, char *format, va_list args);
-int print_float(int n, char *format, va_list args);
-int print_binary(int n, char *format, va_list args);
-int print_hex(int n, char *format, va_list args);
-int print_octal(int n, char *format, va_list args);
-int print_unsigned_int(int n, char *format, va_list args);
-int _is_alpha(char c);
-int _is_specifier(char c);
-unsigned int compute_places(unsigned int n, int step);
-int _strlen(char *s);
-char *rot13(char *str);
-char *_reverse_string(char *str);
-int print_reverse(int n, char *format, va_list args);
-int print_rot13(int n, char *format, va_list args);
-int print_custom_specifier(int n, char *format, va_list args);
-int _is_digit(char c);
-int _atoi(char *s);
-int _pow_recursion(int x, int y);
-int left_align(char *format, int n);
-int right_align(char *format, int n);
-int pad_zero(char *format, int n);
-int count_int(int n);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-char *_strcpy(char *dest, char *src);
-int get_max_width(char *format);
-int get_min_width(char *format);
-int has_precision(char *str);
-int print_string_width(char *format, char *str);
-int print_integer_width(char *format, int n);
-char *to_octal(int n);
-char *to_hex(int n);
-char *_tolower(char *str);
-char get_hex_code(int n);
+	unsigned int unsign			: 1;
+
+	unsigned int plus_flag		: 1;
+	unsigned int space_flag		: 1;
+	unsigned int hashtag_flag	: 1;
+	unsigned int zero_flag		: 1;
+	unsigned int minus_flag		: 1;
+
+	unsigned int width;
+	unsigned int precision;
+
+	unsigned int h_modifier		: 1;
+	unsigned int l_modifier		: 1;
+} params_t;
+
 /**
- * struct hex_code - Struct hex_code
- * @c: The character associated to this code
- * @code: The digit for this char
- * operation.
+ * struct specifier - Struct token
+ *
+ * @specifier: format token
+ * @f: The function associated
  */
-typedef struct hex_code
+typedef struct specifier
 {
-	char c;
-	int code;
-} hex_code_t;
+	char *specifier;
+	int (*f)(va_list, params_t *);
+} specifier_t;
+
+/* _put.c module */
 int _puts(char *str);
-#endif
+int _putchar(int c);
+
+/* print_functions.c module */
+int print_char(va_list ap, params_t *params);
+int print_int(va_list ap, params_t *params);
+int print_string(va_list ap, params_t *params);
+int print_percent(va_list ap, params_t *params);
+int print_S(va_list ap, params_t *params);
+
+/* number.c module */
+char *convert(long int num, int base, int flags, params_t *params);
+int print_unsigned(va_list ap, params_t *params);
+int print_address(va_list ap, params_t *params);
+
+/* specifier.c module */
+int (*get_specifier(char *s))(va_list ap, params_t *params);
+int get_print_func(char *s, va_list ap, params_t *params);
+int get_flag(char *s, params_t *params);
+int get_modifier(char *s, params_t *params);
+char *get_width(char *s, params_t *params, va_list ap);
+
+/* convert_number.c module */
+int print_hex(va_list ap, params_t *params);
+int print_HEX(va_list ap, params_t *params);
+int print_binary(va_list ap, params_t *params);
+int print_octal(va_list ap, params_t *params);
+
+/* simple_printers.c module */
+int print_from_to(char *start, char *stop, char *except);
+int print_rev(va_list ap, params_t *params);
+int print_rot13(va_list ap, params_t *params);
+
+/* print_number.c module */
+int _isdigit(int c);
+int _strlen(char *s);
+int print_number(char *str, params_t *params);
+int print_number_right_shift(char *str, params_t *params);
+int print_number_left_shift(char *str, params_t *params);
+
+/* params.c module */
+void init_params(params_t *params, va_list ap);
+
+/* string_fields.c modoule */
+char *get_precision(char *p, params_t *params, va_list ap);
+
+/* _prinf.c module */
+int _printf(const char *format, ...);
+
+#endif /*_MAIN_H_*/
